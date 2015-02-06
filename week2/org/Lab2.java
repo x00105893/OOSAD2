07db28c9f544ead0cbee80c9bceaa34e3a19162c
x00105893 <x00105893@ittd.ie>
2015-02-06 13:46:07 +0000

    week2 lab2, new method

---
+++ week2/org/Lab2.java
@@ -0,0 +1,25 @@
+package org;
+
+public class Lab2 {
+
+	private int id = 0;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Lab2() {
+		// TODO - implement Lab2.Lab2
+		throw new UnsupportedOperationException();
+	}
+
+	public void print()
+		{
+			system.out.print("hello");
+		}
+
+}
